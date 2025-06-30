0747f235279e4fb3f6e7a6434fd0289d649dfe4b
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-07-01 00:42:29 +0530

    setup the navbar on the dashboard page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,60 @@
+"use client";
 import {
   SignedIn,
   SignedOut,
   SignInButton,
   SignUpButton,
-  UserButton
+  useAuth,
+  UserButton,
 } from "@clerk/nextjs";
 import React from "react";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
+import Link from "next/link";
+
+const routes = [
+  { label: "Instructor", path: "/instructor/courses" },
+  { label: "Learning", path: "/learning" },
+];
 
 const Navbar = () => {
+  const { isSignedIn } = useAuth();
+
   return (
     <div className="flex justify-between items-center bg-white shadow-md h-16">
       <div className="mx-3">
         <h1 className="font-extrabold text-2xl">LMS Platform</h1>
       </div>
+      {isSignedIn && (
+        <div className="flex items-center gap-2 mx-3">
+          <Input
+            type="text"
+            placeholder="Search..."
+            className="border border-gray-300 rounded-md px-3 py-3 w-3xl focus:outline-2 focus:ring-2 focus:ring-blue-500"
+          />
+          <Button className=" cursor-pointer text-white rounded-md p-2 ">
+            <Search />
+          </Button>
+        </div>
+      )}
+      {isSignedIn && (
+        <div className="flex items-center gap-5">
+          {routes.map((item, _idx) => (
+            <Link
+              className="font-semibold hover:underline hover:text-blue-500"
+              key={_idx}
+              href={item.path}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      )}
       <div className="flex justify-end items-center p-4 gap-4 h-16">
-        <SignedOut >
+        <SignedOut>
           <Button variant={"outline"} className="cursor-pointer">
-            <SignInButton forceRedirectUrl="/dashboard"/>
+            <SignInButton forceRedirectUrl="/dashboard" />
           </Button>
           <SignUpButton forceRedirectUrl="/dashboard">
             <Button className=" cursor-pointer">Sign Up</Button>
